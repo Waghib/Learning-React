6125b2bdb042d5153dad7183c52c656b249942c3
Waghib <Waghibahmad30@gmail.com>
2024-07-01 00:54:23 +0500

    Added welcome and greet component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
-import logo from './logo.svg';
+import React, {Component} from 'react';
+// import logo from './logo.svg';
 import './App.css';
+import Greet from './components/Greet';
+import Welcome from './components/Welcome';
 
-function App() {
-  return (
-    <div className="App">
-      
-    </div>
-  );
+class App extends Component {
+  render(){
+    return (
+      <div className="App">
+        <Greet/>
+        <Welcome/>
+      </div>
+    );
+  }
 }
 
 export default App;
