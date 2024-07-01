c02f79e1a7e633b819932b852c84d6824b65fc9c
Waghib <Waghibahmad30@gmail.com>
2024-07-01 17:44:54 +0500

    Understanding difference between props and state

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -14,3 +14,11 @@ export default Welcome;
 // 3. Complex UI logic
 // 4. Provide lifecycle hooks
 // 5. Stateful/Smart/Container
+
+
+//Props vs State
+// 1. props get passed to the component whereas state is managed within the component
+// 2.function parameters vs variables declared in a function
+// 3. props are immutable whereas state can be changed
+// 4. props - functional components   useState hook - functional components
+// this.props - class components      this.state - class components
