a50bdc12a945ada768c7c1b9a1e64bbc4af303d6
Waghib <Waghibahmad30@gmail.com>
2024-07-03 23:34:26 +0500

    Understanding event handler using both function and class components

--- src/App.js
+++ src/App.js
@@ -6,26 +6,31 @@ import Welcome from './components/Welcome';
 // import Hello from './components/Hello';
 import Message from './components/Message';
 import Counter from './components/Counter';
+import FunctionClick from './components/FunctionClick';
+import ClassClick from './components/ClassClick';
 
 class App extends Component {
   render(){
     return (
       <div className="App">
-        <Greet name="Waghib Ahmad" heroName="batman">
+        {/* <Greet name="Waghib Ahmad" heroName="batman">
           <p>This is children props</p>
         </Greet>
-        <Greet name="Bruce" heroName="superman"/>
+        <Greet name="Bruce" heroName="superman"/> */}
         {/* <button>Action</button> */}
         {/* <Greet name="Clark"/> */}
 
 
-        <Welcome name="Diana" heroName="Wonder Woman"/>
-        <Welcome name="Waghib Ahmad" heroName="batman"/>
+        {/* <Welcome name="Diana" heroName="Wonder Woman"/>
+        <Welcome name="Waghib Ahmad" heroName="batman"/> */}
 
         {/* <Message/> */}
         {/* <Counter/> */}
 
         {/* <Hello/> */}
+
+        <FunctionClick/>
+        <ClassClick/>
       </div>
     );
   }
