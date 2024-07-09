98f4a4f2bc209ca4774e13eabfacbf08259d5e69
Waghib <Waghibahmad30@gmail.com>
2024-07-09 19:08:17 +0500

    Understood react fragments

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React, {Component} from 'react';
 import './App.css';
-
+import FragmentDemo from './FragmentDemo';
+import Table from './Table';
 
 class App extends Component {
   render(){
     return (
       <div className="App">
-        
+        {/* <FragmentDemo/> */}
+        <Table/>
       </div>
     );
   }
