c928c6cc85ccec8e9fb253383ed37031fcf58fd8
Waghib <Waghibahmad30@gmail.com>
2024-07-11 00:25:11 +0500

    Understood refs

--- src/App.js
+++ src/App.js
@@ -1,15 +1,14 @@
 import React, {Component} from 'react';
 import './App.css';
-import PureComp from './PureComp';
-import ParentComp from './ParentComp';
+import RefsDemo from './components/RefsDemo';
+
 
 
 class App extends Component {
   render(){
     return (
       <div className="App"> 
-        {/* <PureComp /> */}
-        <ParentComp/>
+        <RefsDemo />
       </div>
     );
   }
---
+++ src/components/RefsDemo.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react'
+
+class RefsDemo extends Component {
+
+    constructor(props) {
+        super(props)
+        this.inputRef = React.createRef()
+        this.cbref = null
+        this.setCbRef = element => {
+            this.cbref = element
+        }
+    }
+
+    componentDidMount() {
+        // this.inputRef.current.focus()
+        // console.log(this.inputRef)
+        if(this.cbref) {
+            this.cbref.focus()
+        }
+    }
+
+    clickHandler = () => {
+        alert(this.inputRef.current.value)
+    }
+
+  render() {  
+    return (
+        <div>
+            <input type="text" ref={this.inputRef} />
+            <input type="text" ref={this.setCbRef} />
+            <button onClick={this.clickHandler}>Click</button>
+        </div>
+    )
+  }
+}
+
+export default RefsDemo
