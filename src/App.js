bda37119359dca19decf307e4e8d7b163cc42753
Waghib <Waghibahmad30@gmail.com>
2024-07-12 00:31:14 +0500

    Understood portals in react

--- src/App.js
+++ src/App.js
@@ -1,8 +1,6 @@
 import React, {Component} from 'react';
 import './App.css';
-import RefsDemo from './components/RefsDemo';
-import FocusInput from './components/FocusInput';
-import FRParentInput from './components/FRParentInput';
+import PortalDemo from './components/PortalDemo';
 
 
 
@@ -10,7 +8,7 @@ class App extends Component {
   render(){
     return (
       <div className="App"> 
-        <FRParentInput />
+        <PortalDemo />
       </div>
     );
   }
