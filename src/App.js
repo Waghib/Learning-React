d95abcf176d75569c348151a8a1caea7a43f46b0
Waghib <Waghibahmad30@gmail.com>
2024-07-16 23:28:10 +0500

    Understood rendering props (part 1)

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import React, {Component} from 'react';
 import './App.css';
-import ClickCounter from './components/ClickCounter';
-import HoverCounter from './components/HoverCounter';
+import ClickCounterTwo from './components/ClickCounterTwo';
+import HoverCounterTwo from './components/HoverCounterTwo';
+
 class App extends Component {
   render(){
     return (
       <div className="App"> 
-        <ClickCounter name='Waghib'/>
-        <HoverCounter />
-
+        <ClickCounterTwo />
+        <HoverCounterTwo />
       </div>
     );
   }
