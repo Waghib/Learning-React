833bbe707dd7f4982c21646701dddc34886b37d0
Waghib <Waghibahmad30@gmail.com>
2024-07-18 00:32:55 +0500

    Understood HTTP GET request

--- src/App.js
+++ src/App.js
@@ -1,17 +1,11 @@
 import React, { Component } from "react";
 import "./App.css";
-import ComponentC from "./components/ComponentC";
-import { UserProvider } from "./components/userContext";
-
-
+import PostList from "./components/PostList";
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <UserProvider value='Waghib'>
-          <ComponentC />
-        </UserProvider>
-
+        <PostList />
       </div>
     );
   }
