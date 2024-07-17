f0c5f1fc8c53aefabea5b4959b2145b7069a0434
Waghib <Waghibahmad30@gmail.com>
2024-07-18 00:14:57 +0500

    Understood Context - other ways to use context (part 2)

--- src/components/ComponentE.js
+++ src/components/ComponentE.js
@@ -1,12 +1,26 @@
 import React, { Component } from 'react'
 import ComponentF from './ComponentF'
+import UserContext from './userContext'
 
 class ComponentE extends Component {
+
+    static contextType = UserContext
+
   render() {
     return (
-      <ComponentF />
+        <div>
+            Component E context {this.context}
+            <ComponentF />
+        </div>
     )
   }
 }
 
+// ComponentE.contextType = UserContext
+
 export default ComponentE
+
+// limitations
+// 1. Only works with class components
+// 2. Only subscribes to a single context using contextType
+
