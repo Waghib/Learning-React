09b8de7f0e6e2942ad4b17c9187e49342c44dbd3
Waghib <Waghibahmad30@gmail.com>
2024-07-18 21:59:29 +0500

    React course completed now starting with react hook tutorials

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
 import React, { Component } from "react";
 import "./App.css";
-import PostForm from "./components/PostForm";
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <PostForm />
       </div>
     );
   }
